ecea805ea0c3bbdcc9d903c17f90cc4bfadbc001
Tim Düsterhus <timwolla@arcor.de>
2013-04-22 15:07:03 +0200

    Add script to installation

---
+++ files/acp/install_de_wbbaddons_dummy_app.php
@@ -0,0 +1,7 @@
+<?php
+// remove dependencies, as they are in the package.xml for the sole reason of installing them all
+$package = $this->installation->getPackage();
+$sql = "DELETE FROM	wcf".WCF_N."_package_requirement
+	WHERE		packageID = ?";
+$statement = WCF::getDB()->prepareStatement($sql);
+$statement->execute(array($package->packageID));
