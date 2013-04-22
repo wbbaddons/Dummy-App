9b0396fcaf7b5fc1ffeb21990a82780d90fc26a0
Tim Düsterhus <timwolla@arcor.de>
2013-04-22 15:08:22 +0200

    Except WCF of course…

--- files/acp/install_de_wbbaddons_dummy_app.php
+++ files/acp/install_de_wbbaddons_dummy_app.php
@@ -2,6 +2,7 @@
 // remove dependencies, as they are in the package.xml for the sole reason of installing them all
 $package = $this->installation->getPackage();
 $sql = "DELETE FROM	wcf".WCF_N."_package_requirement
-	WHERE		packageID = ?";
+	WHERE		packageID = ?
+		AND	requirement <> ?";
 $statement = WCF::getDB()->prepareStatement($sql);
-$statement->execute(array($package->packageID));
+$statement->execute(array($package->packageID, 1));
