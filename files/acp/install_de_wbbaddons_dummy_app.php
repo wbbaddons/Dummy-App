212befaeb1fe3365c4203880b728b4db18d0683a
Tim Düsterhus <timwolla@googlemail.com>
2014-01-26 20:58:55 +0100

    Set default page title

--- files/acp/install_de_wbbaddons_dummy_app.php
+++ files/acp/install_de_wbbaddons_dummy_app.php
@@ -6,3 +6,13 @@ $sql = "DELETE FROM	wcf".WCF_N."_package_requirement
 		AND	requirement <> ?";
 $statement = wcf\system\WCF::getDB()->prepareStatement($sql);
 $statement->execute(array($package->packageID, 1));
+
+// set default page title
+if (!defined('PAGE_TITLE') || !PAGE_TITLE) {
+	$sql = "UPDATE	wcf".WCF_N."_option
+		SET	optionValue = ?
+		WHERE	optionName = ?";
+	$statement = WCF::getDB()->prepareStatement($sql);
+	$statement->execute(array('Dummy App', 'page_title'));
+	\wcf\data\option\OptionEditor::resetCache();
+}
