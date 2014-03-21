7e20457e1e914cdb4e18ade9b7c181fa8955dcc4
Tim Düsterhus <timwolla@googlemail.com>
2014-03-21 17:59:38 +0100

    Fix install script

--- files/acp/install_de_wbbaddons_dummy_app.php
+++ files/acp/install_de_wbbaddons_dummy_app.php
@@ -4,7 +4,7 @@ $package = $this->installation->getPackage();
 $sql = "DELETE FROM	wcf".WCF_N."_package_requirement
 	WHERE		packageID = ?
 		AND	requirement <> ?";
-$statement = wcf\system\WCF::getDB()->prepareStatement($sql);
+$statement = \wcf\system\WCF::getDB()->prepareStatement($sql);
 $statement->execute(array($package->packageID, 1));
 
 // set default page title
@@ -12,7 +12,7 @@ if (!defined('PAGE_TITLE') || !PAGE_TITLE) {
 	$sql = "UPDATE	wcf".WCF_N."_option
 		SET	optionValue = ?
 		WHERE	optionName = ?";
-	$statement = WCF::getDB()->prepareStatement($sql);
+	$statement = \wcf\system\WCF::getDB()->prepareStatement($sql);
 	$statement->execute(array('Dummy App', 'page_title'));
 	\wcf\data\option\OptionEditor::resetCache();
 }
